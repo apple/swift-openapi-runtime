faba5ce7d5ac8b3cefec3a4f8e09ab18e5e78a05
Honza Dvorsky <honza@apple.com>
2023-09-06 18:36:43 +0200

    Add back docs

--- Sources/OpenAPIRuntime/Errors/ClientError.swift
+++ Sources/OpenAPIRuntime/Errors/ClientError.swift
@@ -13,7 +13,6 @@
 //===----------------------------------------------------------------------===//
 
 import HTTPTypes
-
 #if canImport(Darwin)
 import Foundation
 #else
@@ -21,24 +20,65 @@ import Foundation
 @preconcurrency import protocol Foundation.LocalizedError
 #endif
 
+
+/// An error thrown by a client performing an OpenAPI operation.
+///
+/// Use a `ClientError` to inspect details about the request and response 
+/// that resulted in an error.
+///
+/// You don't create or throw instances of `ClientError` yourself; they are 
+/// created and thrown on your behalf by the runtime library when a client
+/// operation fails.
 public struct ClientError: Error {
 
+    /// The identifier of the operation, as defined in the OpenAPI document.
     public var operationID: String
 
+    /// The operation-specific Input value.
     public var operationInput: any Sendable
 
+    /// The HTTP request created during the operation.
+    ///
+    /// Will be nil if the error resulted before the request was generated,
+    /// for example if generating the request from the Input failed.
     public var request: HTTPRequest?
 
+    /// The HTTP request body created during the operation.
+    ///
+    /// Will be nil if the error resulted before the request was generated,
+    /// for example if generating the request from the Input failed.
     public var requestBody: HTTPBody?
 
+    /// The base URL for HTTP requests.
+    ///
+    /// Will be nil if the error resulted before the request was generated,
+    /// for example if generating the request from the Input failed.
     public var baseURL: URL?
 
+    /// The HTTP response received during the operation.
+    ///
+    /// Will be nil if the error resulted before the response was received.
     public var response: HTTPResponse?
 
+    /// The HTTP response body received during the operation.
+    ///
+    /// Will be nil if the error resulted before the response was received.
     public var responseBody: HTTPBody?
 
+    /// The underlying error that caused the operation to fail.
     public var underlyingError: any Error
 
+    /// Creates a new error.
+    /// - Parameters:
+    ///   - operationID: The OpenAPI operation identifier.
+    ///   - operationInput: The operation-specific Input value.
+    ///   - request: The HTTP request created during the operation.
+    ///   - request: The HTTP request body created during the operation.
+    ///   - baseURL: The base URL for HTTP requests.
+    ///   - response: The HTTP response received during the operation.
+    ///   - response: The HTTP response body received during the operation.
+    ///   - underlyingError: The underlying error that caused the operation
+    ///     to fail.
     public init(
         operationID: String,
         operationInput: any Sendable,
