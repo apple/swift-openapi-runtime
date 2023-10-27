f8151aaf00db7e9b1fb5a4afa59c4cbf8b675e11
Honza Dvorsky <honza@apple.com>
2023-10-27 16:04:12 +0200

    Include partial errors in oneOf/anyOf decoding errors

--- Sources/OpenAPIRuntime/Deprecated/Deprecated.swift
+++ Sources/OpenAPIRuntime/Deprecated/Deprecated.swift
@@ -98,3 +98,74 @@ extension ServerError {
         )
     }
 }
+
+extension DecodingError {
+    /// Returns a decoding error used by the oneOf decoder when not a single
+    /// child schema decodes the received payload.
+    /// - Parameters:
+    ///   - type: The type representing the oneOf schema in which the decoding
+    ///   occurred.
+    ///   - codingPath: The coding path to the decoder that attempted to decode
+    ///   the type.
+    /// - Returns: A decoding error.
+    @_spi(Generated)
+    @available(*, deprecated)
+    public static func failedToDecodeOneOfSchema(
+        type: Any.Type,
+        codingPath: [any CodingKey]
+    ) -> Self {
+        DecodingError.valueNotFound(
+            type,
+            DecodingError.Context.init(
+                codingPath: codingPath,
+                debugDescription: "The oneOf structure did not decode into any child schema."
+            )
+        )
+    }
+
+    /// Returns a decoding error used by the anyOf decoder when not a single
+    /// child schema decodes the received payload.
+    /// - Parameters:
+    ///   - type: The type representing the anyOf schema in which the decoding
+    ///   occurred.
+    ///   - codingPath: The coding path to the decoder that attempted to decode
+    ///   the type.
+    /// - Returns: A decoding error.
+    @available(*, deprecated)
+    static func failedToDecodeAnySchema(
+        type: Any.Type,
+        codingPath: [any CodingKey]
+    ) -> Self {
+        DecodingError.valueNotFound(
+            type,
+            DecodingError.Context.init(
+                codingPath: codingPath,
+                debugDescription: "The anyOf structure did not decode into any child schema."
+            )
+        )
+    }
+
+    /// Verifies that the anyOf decoder successfully decoded at least one
+    /// child schema, and throws an error otherwise.
+    /// - Parameters:
+    ///   - values: An array of optional values to check.
+    ///   - type: The type representing the anyOf schema in which the decoding
+    ///   occurred.
+    ///   - codingPath: The coding path to the decoder that attempted to decode
+    ///   the type.
+    /// - Throws: An error of type `DecodingError.failedToDecodeAnySchema` if none of the child schemas were successfully decoded.
+    @_spi(Generated)
+    @available(*, deprecated)
+    public static func verifyAtLeastOneSchemaIsNotNil(
+        _ values: [Any?],
+        type: Any.Type,
+        codingPath: [any CodingKey]
+    ) throws {
+        guard values.contains(where: { $0 != nil }) else {
+            throw DecodingError.failedToDecodeAnySchema(
+                type: type,
+                codingPath: codingPath
+            )
+        }
+    }
+}
