e33fc0d6d973f31b4451090391edfb4335599b0b
Honza Dvorsky <honza@apple.com>
2023-12-15 10:52:39 +0100

    Added SSE decoding, plus JSON data support

--- Sources/OpenAPIRuntime/EventStreams/ServerSentEventsDecoding.swift
+++ Sources/OpenAPIRuntime/EventStreams/ServerSentEventsDecoding.swift
@@ -14,134 +14,243 @@
 
 import Foundation
 
-//struct ServerSentEventsDeserializationSequence<Upstream: AsyncSequence & Sendable>: Sendable where Upstream.Element == ArraySlice<UInt8> {
-//    var upstream: Upstream
-//    init(upstream: Upstream) {
-//        self.upstream = upstream
-//    }
-//}
-//
-//extension ServerSentEventsDeserializationSequence: AsyncSequence {
-//    typealias Element = ArraySlice<UInt8>
-//    
-//    struct Iterator<UpstreamIterator: AsyncIteratorProtocol>: AsyncIteratorProtocol where UpstreamIterator.Element == Element {
-//        var upstream: UpstreamIterator
-//        var stateMachine: ServerSentEventsDeserializerStateMachine = .init()
-//        mutating func next() async throws -> ArraySlice<UInt8>? {
-//            while true {
-//                switch stateMachine.next() {
-//                case .returnNil:
-//                    return nil
-//                case .returnLine(let line):
-//                    return line
-//                case .needsMore:
-//                    let value = try await upstream.next()
-//                    switch stateMachine.receivedValue(value) {
-//                    case .returnNil:
-//                        return nil
-//                    case .returnLine(let line):
-//                        return line
-//                    case .noop:
-//                        continue
-//                    }
-//                }
-//            }
-//        }
-//    }
-//    
-//    func makeAsyncIterator() -> Iterator<Upstream.AsyncIterator> {
-//        Iterator(upstream: upstream.makeAsyncIterator())
-//    }
-//}
+/// https://html.spec.whatwg.org/multipage/server-sent-events.html#server-sent-events
 
-// https://html.spec.whatwg.org/multipage/server-sent-events.html#server-sent-events
+struct ServerSentEventsDeserializationSequence<Upstream: AsyncSequence & Sendable>: Sendable where Upstream.Element == ArraySlice<UInt8> {
+    var upstream: Upstream
+    init(upstream: Upstream) {
+        self.upstream = upstream
+    }
+}
 
-//struct ServerSentEventFrame {
-//    var name: String?
-//    var value: String?
-//}
-//
-//struct ServerSentEvent {
-//    var event: String?
-//    var data: String?
-//    var id: String?
-//    var retry: Int64?
-//}
-//
-//extension AsyncSequence where Element == ArraySlice<UInt8> {
-//    func asDecodedServerSentEvents<Event: Decodable>(
-//        of eventType: Event.Type = Event.self,
-//        using decoder: JSONDecoder = .init()
-//    ) -> AsyncThrowingMapSequence<ServerSentEventsDeserializationSequence<Self>, Event> {
-//        asParsedServerSentEvents().map { line in
-//            try decoder.decode(Event.self, from: Data(line))
-//        }
-//    }
-//}
+extension ServerSentEventsDeserializationSequence: AsyncSequence {
+    typealias Element = ServerSentEvent
+    
+    struct Iterator<UpstreamIterator: AsyncIteratorProtocol>: AsyncIteratorProtocol where UpstreamIterator.Element == ArraySlice<UInt8> {
+        var upstream: UpstreamIterator
+        var stateMachine: ServerSentEventsDeserializerStateMachine = .init()
+        mutating func next() async throws -> ServerSentEvent? {
+            while true {
+                switch stateMachine.next() {
+                case .returnNil:
+                    return nil
+                case .emitEvent(let event):
+                    return event
+                case .noop:
+                    continue
+                case .needsMore:
+                    let value = try await upstream.next()
+                    switch stateMachine.receivedValue(value) {
+                    case .returnNil:
+                        return nil
+                    case .noop:
+                        continue
+                    }
+                }
+            }
+        }
+    }
+    
+    func makeAsyncIterator() -> Iterator<Upstream.AsyncIterator> {
+        Iterator(upstream: upstream.makeAsyncIterator())
+    }
+}
 
-//struct ServerSentEventsDeserializerStateMachine {
-//    
-//    enum State {
-//        case waitingForDelimiter([UInt8])
-//        case finished
-//        case mutating
-//    }
-//    private(set) var state: State = .waitingForDelimiter([])
-//    
-//    enum NextAction {
-//        case returnNil
-//        case returnLine(ArraySlice<UInt8>)
-//        case needsMore
-//    }
-//    
-//    mutating func next() -> NextAction {
-//        switch state {
-//        case .waitingForDelimiter(var buffer):
-//            state = .mutating
-//            guard let indexOfNewline = buffer.firstIndex(of: ASCII.lf) else {
-//                state = .waitingForDelimiter(buffer)
-//                return .needsMore
-//            }
-//            let line = buffer[..<indexOfNewline]
-//            buffer.removeSubrange(...indexOfNewline)
-//            state = .waitingForDelimiter(buffer)
-//            return .returnLine(line)
-//        case .finished:
-//            return .returnNil
-//        case .mutating:
-//            preconditionFailure("Invalid state")
-//        }
-//    }
-//    
-//    enum ReceivedValueAction {
-//        case returnNil
-//        case returnLine(ArraySlice<UInt8>)
-//        case noop
-//    }
-//    
-//    mutating func receivedValue(_ value: ArraySlice<UInt8>?) -> ReceivedValueAction {
-//        switch state {
-//        case .waitingForDelimiter(var buffer):
-//            if let value {
-//                state = .mutating
-//                buffer.append(contentsOf: value)
-//                state = .waitingForDelimiter(buffer)
-//                return .noop
-//            } else {
-//                let line = ArraySlice(buffer)
-//                buffer = []
-//                state = .finished
-//                if line.isEmpty {
-//                    return .returnNil
-//                } else {
-//                    return .returnLine(line)
-//                }
-//            }
-//        case .finished, .mutating:
-//            preconditionFailure("Invalid state")
-//        }
-//    }
-//}
+extension AsyncSequence where Element == ArraySlice<UInt8> {
+
+    func asDecodedServerSentEvents() -> ServerSentEventsDeserializationSequence<ServerSentEventsLineDeserializationSequence<Self>> {
+        .init(upstream: asParsedServerSentEventLines())
+    }
+
+    func asDecodedServerSentEventsWithJSONData<JSONDataType: Decodable>(
+        of dataType: JSONDataType.Type = JSONDataType.self,
+        using decoder: JSONDecoder = .init()
+    ) -> AsyncThrowingMapSequence<
+        ServerSentEventsDeserializationSequence<ServerSentEventsLineDeserializationSequence<Self>>,
+        ServerSentEventWithJSONData<JSONDataType>
+    > {
+        asDecodedServerSentEvents().map { event in
+            ServerSentEventWithJSONData(
+                event: event.event,
+                data: try event.data.flatMap { stringData in
+                    try decoder.decode(JSONDataType.self, from: Data(stringData.utf8))
+                },
+                id: event.id,
+                retry: event.retry
+            )
+        }
+    }
+}
+
+/// An event sent by the server.
+///
+/// https://html.spec.whatwg.org/multipage/server-sent-events.html#event-stream-interpretation
+struct ServerSentEventWithJSONData<JSONDataType: Decodable & Sendable>: Sendable {
+
+    /// A type of the event, helps inform how to interpret the data.
+    var event: String?
+
+    /// The payload of the event.
+    var data: JSONDataType?
+
+    /// A unique identifier of the event, can be used to resume an interrupted stream by
+    /// making a new request with the `Last-Event-ID` header field set to this value.
+    ///
+    /// https://html.spec.whatwg.org/multipage/server-sent-events.html#the-last-event-id-header
+    var id: String?
+
+    /// The amount of time, in milliseconds, the client should wait before reconnecting in case
+    /// of an interruption.
+    ///
+    /// https://html.spec.whatwg.org/multipage/server-sent-events.html#the-eventsource-interface
+    var retry: Int64?
+}
+
+/// An event sent by the server.
+///
+/// https://html.spec.whatwg.org/multipage/server-sent-events.html#event-stream-interpretation
+struct ServerSentEvent: Sendable {
+
+    /// A type of the event, helps inform how to interpret the data.
+    var event: String?
+
+    /// The payload of the event.
+    var data: String?
+
+    /// A unique identifier of the event, can be used to resume an interrupted stream by
+    /// making a new request with the `Last-Event-ID` header field set to this value.
+    ///
+    /// https://html.spec.whatwg.org/multipage/server-sent-events.html#the-last-event-id-header
+    var id: String?
+
+    /// The amount of time, in milliseconds, the client should wait before reconnecting in case
+    /// of an interruption.
+    ///
+    /// https://html.spec.whatwg.org/multipage/server-sent-events.html#the-eventsource-interface
+    var retry: Int64?
+}
+
+struct ServerSentEventsDeserializerStateMachine {
+    
+    enum State {
+        case accumulatingEvent(ServerSentEvent, buffer: [ArraySlice<UInt8>])
+        case finished
+        case mutating
+    }
+    private(set) var state: State = .accumulatingEvent(.init(), buffer: [])
+    
+    enum NextAction {
+        case returnNil
+        case emitEvent(ServerSentEvent)
+        case needsMore
+        case noop
+    }
+    
+    mutating func next() -> NextAction {
+        switch state {
+        case .accumulatingEvent(var event, var buffer):
+            guard let line = buffer.first else {
+                return .needsMore
+            }
+            state = .mutating
+            buffer.removeFirst()
+            if line.isEmpty {
+                // Dispatch the accumulated event.
+                state = .accumulatingEvent(.init(), buffer: buffer)
+                // If the last character of data is a newline, strip it.
+                if event.data?.hasSuffix("\n") ?? false {
+                    event.data?.removeLast()
+                }
+                return .emitEvent(event)
+            }
+            if line[0] == ASCII.colon {
+                // A comment, skip this line.
+                state = .accumulatingEvent(event, buffer: buffer)
+                return .noop
+            }
+            // Parse the field name and value.
+            let field: String
+            let value: String?
+            if let indexOfFirstColon = line.firstIndex(of: ASCII.colon) {
+                field = String(decoding: line[..<indexOfFirstColon], as: UTF8.self)
+                let valueBytes = line[line.index(after: indexOfFirstColon)...]
+                let resolvedValueBytes: ArraySlice<UInt8>
+                if valueBytes.isEmpty {
+                    resolvedValueBytes = []
+                } else if valueBytes[0] == ASCII.space {
+                    resolvedValueBytes = valueBytes.dropFirst()
+                } else {
+                    resolvedValueBytes = valueBytes
+                }
+                value = String(decoding: resolvedValueBytes, as: UTF8.self)
+            } else {
+                field = String(decoding: line, as: UTF8.self)
+                value = nil
+            }
+            guard let value else {
+                // An unknown type of event, skip.
+                state = .accumulatingEvent(event, buffer: buffer)
+                return .noop
+            }
+            // Process the field.
+            switch field {
+            case "event":
+                event.event = value
+            case "data":
+                event.data?.append(value)
+                event.data?.append("\n")
+            case "id":
+                event.id = value
+            case "retry":
+                if let retry = Int64(value) {
+                    event.retry = retry
+                } else {
+                    // Skip this line.
+                    fallthrough
+                }
+            default:
+                // An unknown or invalid field, skip.
+                state = .accumulatingEvent(event, buffer: buffer)
+                return .noop
+            }
+            // Processed the field, continue.
+            state = .accumulatingEvent(event, buffer: buffer)
+            return .noop
+        case .finished:
+            return .returnNil
+        case .mutating:
+            preconditionFailure("Invalid state")
+        }
+    }
+    
+    enum ReceivedValueAction {
+        case returnNil
+        case noop
+    }
+    
+    mutating func receivedValue(_ value: ArraySlice<UInt8>?) -> ReceivedValueAction {
+        switch state {
+        case .accumulatingEvent(let event, var buffer):
+            if let value {
+                state = .mutating
+                buffer.append(value)
+                state = .accumulatingEvent(event, buffer: buffer)
+                return .noop
+            } else {
+                // If no value is received, drop the existing event on the floor.
+                // The specification explicitly states this.
+                // > Once the end of the file is reached, any pending data must be discarded.
+                // > (If the file ends in the middle of an event, before the final empty line,
+                // > the incomplete event is not dispatched.)
+                // Source: https://html.spec.whatwg.org/multipage/server-sent-events.html#event-stream-interpretation
+                state = .finished
+                return .returnNil
+            }
+        case .finished, .mutating:
+            preconditionFailure("Invalid state")
+        }
+    }
+}
 
 struct ServerSentEventsLineDeserializationSequence<Upstream: AsyncSequence & Sendable>: Sendable where Upstream.Element == ArraySlice<UInt8> {
     var upstream: Upstream
