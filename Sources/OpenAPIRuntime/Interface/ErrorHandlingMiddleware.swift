7201b6121a95969e778c3957026cd8be2f928aad
gayathrisairam <168187165+gayathrisairam@users.noreply.github.com>
2024-11-11 16:15:47 +0000

    Update Sources/OpenAPIRuntime/Interface/ErrorHandlingMiddleware.swift
    Co-authored-by: Honza Dvorsky <honza@apple.com>

--- Sources/OpenAPIRuntime/Interface/ErrorHandlingMiddleware.swift
+++ Sources/OpenAPIRuntime/Interface/ErrorHandlingMiddleware.swift
@@ -14,7 +14,7 @@
 
 import HTTPTypes
 
-/// An opt-in error Handling middleware that converts an error to a HTTP response.
+/// An opt-in error handling middleware that converts an error to a HTTP response.
 ///
 /// Inclusion of  ``ErrorHandlingMiddleware`` should be accompanied by confirming errors to ``HTTPResponseConvertible``  protocol.
 /// Undocumented errors, i.e, errors not confriming to ``HTTPResponseConvertible`` are converted to a response with 500 status code.
