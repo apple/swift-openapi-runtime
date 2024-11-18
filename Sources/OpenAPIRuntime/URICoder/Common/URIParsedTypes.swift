15d3124222995e716dd8637cac130ed8e5085be2
Honza Dvorsky <czechboy0@gmail.com>
2024-11-18 13:55:32 +0100

    Update Sources/OpenAPIRuntime/URICoder/Common/URIParsedTypes.swift
    Co-authored-by: Si Beaumont <simonjbeaumont@gmail.com>

--- Sources/OpenAPIRuntime/URICoder/Common/URIParsedTypes.swift
+++ Sources/OpenAPIRuntime/URICoder/Common/URIParsedTypes.swift
@@ -60,29 +60,7 @@ typealias URIParsedPairArray = [URIParsedPair]
 // MARK: - Extensions
 
 extension URIParsedKey: CustomStringConvertible {
-    /// A textual representation of this instance.
-    ///
-    /// Calling this property directly is discouraged. Instead, convert an
-    /// instance of any type to a string by using the `String(describing:)`
-    /// initializer. This initializer works with any type, and uses the custom
-    /// `description` property for types that conform to
-    /// `CustomStringConvertible`:
-    ///
-    ///     struct Point: CustomStringConvertible {
-    ///         let x: Int, y: Int
-    ///
-    ///         var description: String {
-    ///             return "(\(x), \(y))"
-    ///         }
-    ///     }
-    ///
-    ///     let p = Point(x: 21, y: 30)
-    ///     let s = String(describing: p)
-    ///     print(s)
-    ///     // Prints "(21, 30)"
-    ///
-    /// The conversion of `p` to a string in the assignment to `s` uses the
-    /// `Point` type's `description` property.
+    // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
     var description: String {
         if components.isEmpty { return "<empty>" }
         return components.joined(separator: "/")
