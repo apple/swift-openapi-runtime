86cb07383213fb204123cf13555fbb7bbc288221
Mads Odgaard <mads.odgaard@frameo.com>
2026-03-02 12:45:15 +0100

    add linkage test

---
+++ Tests/LinkageTest/Sources/linkageTest/main.swift
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftOpenAPIGenerator open source project
+//
+// Copyright (c) 2026 Apple Inc. and the SwiftOpenAPIGenerator project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import OpenAPIRuntime
+
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import Foundation
+#endif
+
+print("\(URL.defaultOpenAPIServerURL)")
