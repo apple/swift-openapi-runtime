aad1067d0ead7f336a78d17b29323bccce3079e4
Rick Newton-Rogers <rnro@apple.com>
2026-04-01 16:57:04 +0100

    Drop Swift 6.0
    Motivation:
    
    Swift 6.0 is no longer supported, we should bump the tools version and remove it from our CI.
    
    Modifications:
    
    * Bump the Swift tools version to Swift 6.1
    * Remove Swift 6.0 jobs where appropriate in main.yml, pull_request.yml
    
    Result:
    
    Code reflects our support window.

--- Sources/OpenAPIRuntime/Multipart/MultipartFramesToRawPartsSequence.swift
+++ Sources/OpenAPIRuntime/Multipart/MultipartFramesToRawPartsSequence.swift
@@ -348,7 +348,6 @@ extension MultipartFramesToRawPartsSequence {
             case .fetchFrame:
                 let frame: Upstream.AsyncIterator.Element?
                 var upstream = upstream
-                #if compiler(>=6.0)
                 if #available(macOS 15, iOS 18.0, tvOS 18.0, watchOS 11.0, macCatalyst 18.0, visionOS 2.0, *) {
                     frame = try await upstream.next(isolation: self)
                 } else {
@@ -356,9 +355,6 @@ extension MultipartFramesToRawPartsSequence {
                     frame = try await unsafeUpstream.next()
                     upstream = unsafeUpstream
                 }
-                #else
-                frame = try await upstream.next()
-                #endif
                 self.upstream = upstream
                 switch stateMachine.partReceivedFrame(frame) {
                 case .returnNil: return nil
@@ -382,7 +378,6 @@ extension MultipartFramesToRawPartsSequence {
             case .fetchFrame:
                 let frame: Upstream.AsyncIterator.Element?
                 var upstream = upstream
-                #if compiler(>=6.0)
                 if #available(macOS 15, iOS 18.0, tvOS 18.0, watchOS 11.0, macCatalyst 18.0, visionOS 2.0, *) {
                     frame = try await upstream.next(isolation: self)
                 } else {
@@ -390,9 +385,6 @@ extension MultipartFramesToRawPartsSequence {
                     frame = try await unsafeUpstream.next()
                     upstream = unsafeUpstream
                 }
-                #else
-                frame = try await upstream.next()
-                #endif
                 self.upstream = upstream
                 switch stateMachine.bodyReceivedFrame(frame) {
                 case .returnNil: return nil
