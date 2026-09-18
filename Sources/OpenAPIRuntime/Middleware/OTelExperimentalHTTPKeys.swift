dc50a097084a70a01f202e4c9c37411609fbb6e7
Andreas Bauer <andreas-bauer@apple.com>
2026-09-18 13:30:04 +0200

    Add a new ClientOTelLoggingMiddleware and ServerOTelLoggingMiddleware

---
+++ Sources/OpenAPIRuntime/Middleware/OTelExperimentalHTTPKeys.swift
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftOpenAPIGenerator open source project
+//
+// Copyright (c) 2025 Apple Inc. and the SwiftOpenAPIGenerator project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if OTelSemanticConventions
+
+enum OTelExperimentalHTTPKeys {
+    static let httpRequestBodySize = "http.request.body.size"
+    static let httpResponseBodySize = "http.response.body.size"
+}
+
+#endif
